729806e400aeeb724545d8bc1c6858a94865db88
malijaffri <75521399+malijaffri@users.noreply.github.com>
2023-10-03 02:20:00 +0500

    Added support for "Tree Style Tabs"
    Got idea from mkopec/close-tabs-to-the-right

--- background.js
+++ background.js
@@ -26,3 +26,30 @@ browser.contextMenus.onClicked.addListener(function (info, sender) {
 	const querying = browser.tabs.query(tabsQuery);
 	querying.then(copyUrl.bind(null, sender));
 });
+
+// supports Tree Style Tab's fake context menu
+// https://addons.mozilla.org/firefox/addon/tree-style-tab/
+function registerToTST() {
+	browser.runtime
+		.sendMessage("treestyletab@piro.sakura.ne.jp", {
+			type: "fake-contextMenu-create",
+			params: menuItem,
+		})
+		.then(createdMenuItem, createdMenuItem);
+}
+browser.runtime.onMessageExternal.addListener((message, sender) => {
+	switch (sender.id) {
+		case "treestyletab@piro.sakura.ne.jp":
+			switch (message.type) {
+				case "ready":
+					registerToTST();
+					return;
+				case "fake-contextMenu-click":
+					browser.tabs
+						.query(tabsQuery)
+						.then((tabs) => copyUrl(message.tab, tabs));
+					return;
+			}
+	}
+});
+registerToTST();
